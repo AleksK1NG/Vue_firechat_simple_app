f4f97e4a97ff7be1b91c852a98e0270d0bc0717a
Alexander Bryksin <alexander.bryksin@yandex.ru>
2017-12-14 15:58:15 +0300

    vue firechat app

---
+++ src/config.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+const config = {
+  apiKey: "AIzaSyCQMajL8JKiRonxQ_TbSVJ7KifIlhtsask",
+  authDomain: "vue-firestone-chat.firebaseapp.com",
+  databaseURL: "https://vue-firestone-chat.firebaseio.com",
+  projectId: "vue-firestone-chat",
+  storageBucket: "",
+  messagingSenderId: "270702648776"
+};
+
+export default config;
